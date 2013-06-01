4822195d9b4434e32c1125042c48b816c7f8f4ea
Matthew Keefe <mkeefe@pixelbitcreative.com>
2013-05-31 23:14:23 -0400

    Initial Xcode project and some UI elements added

---
+++ src/HappyPeg/HappyPeg/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HappyPeg
+//
+//  Created by mkeefe on 5/31/13.
+//  Copyright (c) 2013 PixelBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ src/HappyPeg/HappyPeg/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HappyPeg
+//
+//  Created by mkeefe on 5/31/13.
+//  Copyright (c) 2013 PixelBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
