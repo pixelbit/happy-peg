cbe3d30fa2b8b32ef3bf1c0a992a6e52e4bd2703
Matthew Keefe <mkeefe@pixelbitcreative.com>
2013-06-11 23:40:00 -0400

    Settings UI

---
+++ src/HappyPeg/HappyPeg/SettingsViewController.h
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.h
+//  HappyPeg
+//
+//  Created by mkeefe on 6/11/13.
+//  Copyright (c) 2013 PixelBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+- (IBAction)back:(id)sender;
+
+@end
