8c91ce491e079de3054cf8475b07ad485340ff91
Matthew Keefe <mkeefe@pixelbitcreative.com>
2013-06-11 23:39:53 -0400

    How to Play

---
+++ src/HappyPeg/HappyPeg/HowToPlayViewController.h
@@ -0,0 +1,15 @@
+//
+//  HowToPlayViewController.h
+//  HappyPeg
+//
+//  Created by mkeefe on 6/11/13.
+//  Copyright (c) 2013 PixelBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HowToPlayViewController : UIViewController
+
+- (IBAction)back:(id)sender;
+
+@end
---
+++ src/HappyPeg/HappyPeg/HowToPlayViewController.m
@@ -0,0 +1,39 @@
+//
+//  HowToPlayViewController.m
+//  HappyPeg
+//
+//  Created by mkeefe on 6/11/13.
+//  Copyright (c) 2013 PixelBit. All rights reserved.
+//
+
+#import "HowToPlayViewController.h"
+
+@interface HowToPlayViewController ()
+
+@end
+
+@implementation HowToPlayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (IBAction)back:(id)sender {
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
